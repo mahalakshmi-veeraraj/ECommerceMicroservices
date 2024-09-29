cf7d7689ed6d3ec66087bb896c3b3a8fdc096906
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-09-29 22:02:05 +0530

    Update PaymentGateway.java

--- payment-service/src/main/java/com/maha/ecommerce/payment_service/paymentGateway/PaymentGateway.java
+++ payment-service/src/main/java/com/maha/ecommerce/payment_service/paymentGateway/PaymentGateway.java
@@ -3,5 +3,5 @@ package com.maha.ecommerce.payment_service.paymentGateway;
 import com.razorpay.RazorpayException;
 
 public interface PaymentGateway {
-	String generatePaymentLink(Long id, Long amount) throws RazorpayException;
+	String generatePaymentLink(Long id, Long amount) throws RazorpayException, StripeException;
 }
